f6c305da89562c8d05d068b2b86a411da771edd2
Juan Pablo Rodriguez <jprodriguez@dcathenea.local>
2022-12-09 15:26:08 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import UsuariosRouter from './routes/UsuariosRouter.js';
+
+
+//Creacion de la app
+const app =express();
+
+//Routing
+app.use('/auth',UsuariosRouter)
+
+//Habilitar Pug
+
+app.set('view engine','pug')
+app.set('views', './views')
+
+
+//Definicion de puerto de arranque
+
+const port=3000;
+app.listen(port,()=>{
+    console.log(`El servidor esta funcionando en el puerto ${port}`)
+});
